6e3bfc5e0a5d7bb0b20a4810b4010f6a20325d53
Syntax Designer <akpanjoseph2021@gmail.com>
2023-08-17 03:33:54 +0100

    added logo image

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="siri" handle="@siri01" />
+      <ProfileCard title="Alexa" handle="@alexa99" imageUrl={'images/alexa.png'} imageAlt="Alexa image" />
+      <ProfileCard title="Cortana" handle="@cortana32" imageUrl={'images/cortana.png'} imageAlt="cortana image" />
+      <ProfileCard title="siri" handle="@siri01" imageUrl={'images/siri.png'} imageAlt="siri image" />
     </div>
   );
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const ProfileCard = (props) => {
-    const {title,handle} = props
+    const {title,handle,imageUrl,imageAlt} = props
     return (
         <div>
+            <img src={imageUrl} alt={imageAlt} />
            <p> Title is {title}</p>
            <p>Handle is {handle} </p>
         </div>

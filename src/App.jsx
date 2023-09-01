4d91a86f348f2751ddbf637e679b185fdecd7a7b
Syntax Designer <akpanjoseph2021@gmail.com>
2023-09-01 03:37:08 +0100

    done with course firtst project

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,34 @@ import ProfileCard from './ProfileCard';
 
 const App = () => {
   return (
-    <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" imageUrl={'images/alexa.png'} imageAlt="Alexa image" />
-      <ProfileCard title="Cortana" handle="@cortana32" imageUrl={'images/cortana.png'} imageAlt="cortana image" />
-      <ProfileCard title="siri" handle="@siri01" imageUrl={'images/siri.png'} imageAlt="siri image" />
+    <div className='container'>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      
+      
+      <div className="section">
+        <div className="columns">
+
+          <div className="column is-4">
+          <ProfileCard title="Alexa" handle="@alexa99" imageUrl={'images/alexa.png'} imageAlt="Alexa image" description="Cortana was made by Microsoft , who knows what it does?" />
+          </div>
+
+          <div className="column is-4">
+          <ProfileCard title="Cortana" handle="@cortana32" imageUrl={'images/cortana.png'} imageAlt="cortana image" description="siri was made by apple and is being phased out" />
+          </div>
+
+          <div className="column is-4">
+          <ProfileCard title="siri" handle="@siri01" imageUrl={'images/siri.png'} imageAlt="siri image" description='hello' />
+          </div>
+        </div>
+      </div>
+      
+      
+     
+     
     </div>
   );
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const ProfileCard = (props) => {
-    const {title,handle,imageUrl,imageAlt} = props
-    return (
-        <div>
-            <img src={imageUrl} alt={imageAlt} />
-           <p> Title is {title}</p>
-           <p>Handle is {handle} </p>
+  const { title, handle, imageUrl, imageAlt, description} = props;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={imageUrl} alt={imageAlt} />
+        </figure> 
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+        <p className="title is-4">  {title}</p>
+        <p className="subtitle is-6"> {handle} </p>
         </div>
-    );
-}
+      <div className="content">{description}</div>
+      </div>
+    </div>
+  );
+};
 
 export default ProfileCard;
